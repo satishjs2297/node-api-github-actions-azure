e9391c9b20ab4b5d70560ee8a8e35e263c3877c3
Satish Y <satishjs2297@gmail.com>
2024-02-25 15:31:43 +0530

    clear

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import * as express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/ping', (req, res) => {
+    res.send('Pong!!! I am active..')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
