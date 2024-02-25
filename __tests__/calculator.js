2ff883b20b3755b2e635db9e37908e84ebc77d13
Satish Y <satishjs2297@gmail.com>
2024-02-25 16:12:23 +0530

    disable unit test

--- __tests__/calculator.js
+++
@@ -1,8 +0,0 @@
-describe("Testing with Jest", () => { 
-    it("Addition", () => { 
-        const sum = 2 + 3; 
-        const expectedResult = 5; 
-        expect(sum).toEqual(expectedResult); 
-    }); 
-});
-    
