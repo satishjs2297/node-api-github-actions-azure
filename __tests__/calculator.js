5bac527c5029ad9dff9bb24496b6c8a9df08c677
Satish Y <satishjs2297@gmail.com>
2024-02-25 15:57:56 +0530

    add jest unit test case

---
+++ __tests__/calculator.js
@@ -0,0 +1,8 @@
+describe("Testing with Jest", () => { 
+    it("Addition", () => { 
+        const sum = 2 + 3; 
+        const expectedResult = 5; 
+        expect(sum).toEqual(expectedResult); 
+    }); 
+});
+    
